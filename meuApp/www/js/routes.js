bff71cc71b3c5ebc510d9454e71ff5157fad298e
Lucas Morais Lopes <140353835+LucasMoraisIT@users.noreply.github.com>
2024-11-12 22:00:39 +0000

    Configuração da pagina inicial

--- meuApp/www/js/routes.js
+++ meuApp/www/js/routes.js
@@ -30,6 +30,61 @@ var app = new Framework7({
 		},
 		pageInit: function (event, page) {
 		// fazer algo quando a página for inicializada
+    app.views.main.router.navigate('/detalhes/');
+    $.getScript('js/index.js');
+    
+
+      var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        autoplay: true,
+        delay: 2000,
+        loop: true,
+        breakpoints:{
+          50:{
+            slidesPerView: 1,
+            spaceBetween: 30
+          },
+          640:{
+            slidesPerView: 2,
+            spaceBetween: 30
+          },
+          992:{
+            slidesPerView: 3,
+            spaceBetween: 30
+          },
+          1200:{
+            slidesPerView: 4,
+            spaceBetween:30
+          }
+        }
+      });
+      var swiper2 = new Swiper(".categorias", {
+        slidesPerView: 3,
+        spaceBetween: 10,
+        breakpoints:{
+          50:{
+            slidesPerView: 3,
+            spaceBetween: 10
+          },
+          640:{
+            slidesPerView: 6,
+            spaceBetween: 10
+          },
+          992:{
+            slidesPerView: 8,
+            spaceBetween: 10
+          },
+          1200:{
+            slidesPerView: 12,
+            spaceBetween: 10
+          }
+        }
+      });
 		},
 		pageBeforeRemove: function (event, page) {
 		// fazer algo antes da página ser removida do DOM
@@ -77,6 +132,25 @@ var app = new Framework7({
     {
       path: '/link4/',
       url: 'link4.html',
+      animate: false,
+	  on: {
+		pageBeforeIn: function (event, page) {
+		// fazer algo antes da página ser exibida
+		},
+		pageAfterIn: function (event, page) {
+		// fazer algo depois da página ser exibida
+		},
+		pageInit: function (event, page) {
+		// fazer algo quando a página for inicializada
+		},
+		pageBeforeRemove: function (event, page) {
+		// fazer algo antes da página ser removida do DOM
+		},
+	  }
+    },
+    {
+      path: '/detalhes/',
+      url: 'detalhes.html',
       animate: false,
 	  on: {
 		pageBeforeIn: function (event, page) {
